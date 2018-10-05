bbb2df2bbe3ce12e36d3d3dd4768d7bd1810fddb
jackvnimble <jack.wilson.v@gmail.com>
2018-10-05 12:19:22 -0400

    Refactor problem3 to use hex_xor over str_xor

--- src/crustopals/problem3.rs
+++ src/crustopals/problem3.rs
@@ -2,19 +2,33 @@ extern crate hex;
 
 use crustopals::tools::*;
 use crustopals::*;
+use std::borrow::Cow;
 use std::cmp::Ordering;
 
-pub fn solve_single_byte_xor(xord_str: &str) -> String {
-  let result: (f32, String, String) = (0u8..128)
-    .into_iter()
-    .map(|u8num| (u8num as char).to_string())
-    .map(|key| (tools::xor_string(xord_str, &key), key))
-    .map(|(pt, key)| (freq_analysis::english_distance(&pt), pt, key))
-    .min_by(|(d1, _, _), (d2, _, _)| {
-      d1.partial_cmp(d2).unwrap_or(Ordering::Equal)
-    }).unwrap();
+// TODO: convert to returning a result for problem 4 iteration purposes
 
-  result.1
+pub fn solve_single_byte_xor(hex_str: &str) -> (f32, String) {
+  let hex_results = generate_single_byte_hex_xors(hex_str);
+
+  let mut english_results: Vec<String> = vec![];
+  for hex in hex_results {
+    let decoded_hex = hex::decode(hex).unwrap();
+    english_results.push(String::from_utf8_lossy(&decoded_hex).to_string());
+  }
+
+  english_results
+    .into_iter()
+    .map(|pt| (freq_analysis::english_distance(&pt), pt))
+    .min_by(|(d1, _), (d2, _)| d1.partial_cmp(d2).unwrap_or(Ordering::Equal))
+    .unwrap()
+}
+
+fn generate_single_byte_hex_xors(hex_str: &str) -> Vec<String> {
+  (0..256)
+    .into_iter()
+    .map(|byte| format!("{:x}", byte))
+    .map(|hex_key| tools::xor_hex(hex_str, &hex_key))
+    .collect()
 }
 
 #[cfg(test)]
@@ -25,22 +39,17 @@ mod tests {
   fn solves_simple_xor() {
     let plaintext = "Hello World";
     let ciphertext = tools::xor_string(plaintext, "a");
-    let deciphered_string = problem3::solve_single_byte_xor(&ciphertext);
-
-    assert_eq!(deciphered_string, plaintext);
+    let ciphertext_hex = hex::encode(ciphertext);
+    let deciphered_string = problem3::solve_single_byte_xor(&ciphertext_hex);
+    assert_eq!(deciphered_string.1, plaintext);
   }
 
   #[test]
   fn solves_problem3() {
-    let hexstring =
+    let ciphertext =
       "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    let ciphertext: String = hex::decode(hexstring)
-      .unwrap()
-      .into_iter()
-      .map(|b| b as char)
-      .collect();
     let deciphered_string = problem3::solve_single_byte_xor(&ciphertext);
 
-    assert_eq!(deciphered_string, "Cooking MC's like a pound of bacon");
+    assert_eq!(deciphered_string.1, "Cooking MC's like a pound of bacon");
   }
 }
