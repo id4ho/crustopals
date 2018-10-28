a1f6dee2f144bb8aabdfdb0795e87a13da55d8b6
jackvnimble <jack.wilson.v@gmail.com>
2018-10-28 13:01:50 -0400

    Mostly wip, breaks first block
    Need to expand code to continue cracking remaining blocks.
    
    This is a pretty neat attack so far. The first byte strategy is pretty
    straightforward. submit 15 bytes and you know that the last byte of the
    first block will be the first byte of the hidden message (the one you
    don't know). Then submit 16 bytes built from the same 15 and another
    byte (iterate through all u8s). You can then try to match the first
    block of the new ciphertext with the old and when you get a match you
    have the first byte of the hidden message.
    
    For the second, you need to submit 14 bytes so that 2 bytes of the
    hidden message are pulled into the first block. Then submit the same 14
    bytes + the previously discovered plaintext byte + a random u8 (again
    iterate through all u8's). When you get the same ciphertext back you've
    found the second byte. Repeat until you have the entire first block.

--- src/crustopals/problem12.rs
+++ src/crustopals/problem12.rs
@@ -2,44 +2,88 @@ extern crate base64;
 
 use crustopals::problem11;
 use crustopals::tools::*;
+use std::iter;
 
 lazy_static! {
   pub static ref RANDOM_KEY: Vec<u8> = aes::generate_key();
-  pub static ref APPEND_STR: String = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK".to_string();
+  pub static ref APPEND_STR: String = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWct\
+dG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyB\
+qdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK".to_string();
 }
 
-pub fn aes_128_ecb_rand_key_oracle(message: String) -> Vec<u8> {
-  let mut plaintext: Vec<u8> = message.as_bytes().to_vec();
-  plaintext.extend(base64::decode(&APPEND_STR.to_string()).unwrap());
-  aes::encrypt_message_ecb(&plaintext, &RANDOM_KEY.to_vec())
+pub fn aes_128_ecb_rand_key_oracle(mut message: Vec<u8>) -> Vec<u8> {
+  // let mut plaintext: Vec<u8> = message.as_bytes().to_vec();
+  message.extend(base64::decode(&APPEND_STR.to_string()).unwrap());
+  aes::encrypt_message_ecb(&message, &RANDOM_KEY.to_vec())
 }
 
 pub fn discover_blocksize() -> usize {
-  let test_byte = "A".to_string();
-  let ciphertext_len1 = aes_128_ecb_rand_key_oracle(test_byte.repeat(1)).len();
+  let test_byte: u8 = 65;
+  let ciphertext_len1 =
+    aes_128_ecb_rand_key_oracle(build_byte_vec(test_byte, 1)).len();
   let mut ciphertext_len2 =
-    aes_128_ecb_rand_key_oracle(test_byte.repeat(2)).len();
+    aes_128_ecb_rand_key_oracle(build_byte_vec(test_byte, 2)).len();
   let mut repeats = 2;
   while ciphertext_len2 <= ciphertext_len1 {
     repeats = repeats + 1;
-    let pt = test_byte.repeat(repeats);
+    let pt = build_byte_vec(test_byte, repeats);
     ciphertext_len2 = aes_128_ecb_rand_key_oracle(pt).len();
   }
   ciphertext_len2 - ciphertext_len1
 }
 
 pub fn test_ecb() -> bool {
-  let test_str =
+  let test_bytes =
     "two blocks of message repeating two blocks of message repeating "
-      .to_string();
-  let ciphertext = aes_128_ecb_rand_key_oracle(test_str);
+      .to_string()
+      .as_bytes()
+      .to_vec();
+  let ciphertext = aes_128_ecb_rand_key_oracle(test_bytes);
   let mode: String = problem11::aes_ecb_cbc_oracle(&ciphertext);
   "ecb".to_string() == mode
+}
+
+pub fn crack_the_aes_ecb_oracle() -> Vec<u8> {
+  let block_size = discover_blocksize();
+  let mut recovered_pt: Vec<u8> = vec![];
+  while recovered_pt.len() < 16 {
+    let mut oracle_msg =
+      build_byte_vec(65, block_size - 1 - recovered_pt.len());
+    let ct_block = &aes_128_ecb_rand_key_oracle(oracle_msg.clone())[0..16];
+    oracle_msg.extend(recovered_pt.to_vec());
+    recovered_pt.push(find_next_pt_byte(oracle_msg, ct_block).unwrap());
+  }
+  println!("{}", bytes_to_string(recovered_pt.clone()));
+  recovered_pt
+}
+
+fn find_next_pt_byte(oracle_msg: Vec<u8>, block: &[u8]) -> Result<u8, &str> {
+  println!("{:?}", oracle_msg);
+  for byte in 0u8..=255 {
+    let mut msg = oracle_msg.clone();
+    msg.push(byte);
+    let ct = aes_128_ecb_rand_key_oracle(msg);
+    if block == &ct[0..16] {
+      return Ok(byte);
+    }
+  }
+  Err("Failed to find byte :(")
+}
+
+fn build_byte_vec(byte: u8, size: usize) -> Vec<u8> {
+  iter::repeat(byte).take(size).collect::<Vec<u8>>()
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  #[test]
+  fn cracks_the_oracle() {
+    crack_the_aes_ecb_oracle();
+
+    assert_eq!(2, 3);
+  }
 
   #[test]
   fn can_determine_blocksize() {
@@ -49,12 +93,5 @@ mod tests {
   #[test]
   fn confirms_ecb_mode() {
     assert_eq!(test_ecb(), true);
-  }
-
-  #[test]
-  fn view_rand_key() {
-    aes_128_ecb_rand_key_oracle("OK".to_string());
-
-    assert_eq!(2, 3);
   }
 }
