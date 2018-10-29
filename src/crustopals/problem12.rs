ee5559003fc0f525d9bfc5fdc53b0e8c839f023e
jackvnimble <jack.wilson.v@gmail.com>
2018-10-29 11:19:14 -0400

    Modify cracking function to recover all blocks
    This involved some changes to modulo 16 the indexes to ensure I was
    operating on the correct block as the while loop progressed. Getting the
    main loop correct was a bit weird and the while condition may look
    wrong.. and it would be wrong for certain plaintexts (those including
    raw 0x01).
    
    I believe a more accurate solution would likely continue to decrypt
    until you couldn't find the next character.. This would indicate that
    the character had changed when you submitted a different length oracle
    message. I.e. you're at the end of the plaintext message and trying to
    recover padding, which is changing as you adjust the length of input.
    (i.e 0x01 -> 0x02 and now solving for the same block is impossible). I
    will likely copy this code and update it for challenge 14 since it's a
    related attack.

--- src/crustopals/problem12.rs
+++ src/crustopals/problem12.rs
@@ -12,7 +12,6 @@ qdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK".to_string();
 }
 
 pub fn aes_128_ecb_rand_key_oracle(mut message: Vec<u8>) -> Vec<u8> {
-  // let mut plaintext: Vec<u8> = message.as_bytes().to_vec();
   message.extend(base64::decode(&APPEND_STR.to_string()).unwrap());
   aes::encrypt_message_ecb(&message, &RANDOM_KEY.to_vec())
 }
@@ -46,24 +45,39 @@ pub fn test_ecb() -> bool {
 pub fn crack_the_aes_ecb_oracle() -> Vec<u8> {
   let block_size = discover_blocksize();
   let mut recovered_pt: Vec<u8> = vec![];
-  while recovered_pt.len() < 16 {
-    let mut oracle_msg =
-      build_byte_vec(65, block_size - 1 - recovered_pt.len());
-    let ct_block = &aes_128_ecb_rand_key_oracle(oracle_msg.clone())[0..16];
-    oracle_msg.extend(recovered_pt.to_vec());
-    recovered_pt.push(find_next_pt_byte(oracle_msg, ct_block).unwrap());
+  let mut found_padding: bool = false;
+  while !found_padding {
+    // this is a bit hard to wrap your head around, but when the entire
+    // plaintext has been recovered we'll start "recovering" padding. The first
+    // byte of padding we recover is naturally 0x01 since we are recovering
+    // bytes in the last position of a block (and everything before it will be
+    // actual plaintext). There may be a subtle bug here if the plaintext had a
+    // literal 0x01 in it.. but we can cross that bridge if we come to it.
+    if let Some(&1u8) = recovered_pt.last() {
+      found_padding = true;
+      recovered_pt.pop();
+    } else {
+      let mut oracle_msg =
+        build_byte_vec(65, block_size - 1 - (recovered_pt.len() % 16));
+      let ct = &aes_128_ecb_rand_key_oracle(oracle_msg.clone());
+      let blk_num = recovered_pt.len() / 16;
+      let relevant_ct_block = &ct[(blk_num * 16)..((blk_num + 1) * 16)];
+      oracle_msg.extend(recovered_pt.to_vec());
+      recovered_pt
+        .push(find_next_pt_byte(oracle_msg, relevant_ct_block).unwrap());
+    }
   }
-  println!("{}", bytes_to_string(recovered_pt.clone()));
   recovered_pt
 }
 
 fn find_next_pt_byte(oracle_msg: Vec<u8>, block: &[u8]) -> Result<u8, &str> {
   println!("{:?}", oracle_msg);
+  let blk_num = oracle_msg.len() / 16;
   for byte in 0u8..=255 {
     let mut msg = oracle_msg.clone();
     msg.push(byte);
     let ct = aes_128_ecb_rand_key_oracle(msg);
-    if block == &ct[0..16] {
+    if block == &ct[(blk_num * 16)..((blk_num + 1) * 16)] {
       return Ok(byte);
     }
   }
@@ -80,9 +94,10 @@ mod tests {
 
   #[test]
   fn cracks_the_oracle() {
-    crack_the_aes_ecb_oracle();
+    let recovered_pt_bytes = crack_the_aes_ecb_oracle();
+    let recovered_pt = bytes_to_string(recovered_pt_bytes);
 
-    assert_eq!(2, 3);
+    assert_eq!(recovered_pt, "Rollin' in my 5.0\nWith my rag-top down so my hair can blow\nThe girlies on standby waving just to say hi\nDid you stop? No, I just drove by\n");
   }
 
   #[test]
